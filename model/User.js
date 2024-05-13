09a748e5194e453b4e2f2f69dd72600755f76ca1
Fábio Rodrigues <fabiojob2023@gmail.com>
2024-05-13 00:14:10 -0300

    feat: conection API table Users

---
+++ model/User.js
@@ -0,0 +1,15 @@
+class UserModel {
+    constructor() {
+        this.apiUrl = "https://66416e463d66a67b3433d99d.mockapi.io/Users";
+    }
+
+    createUser(userData) {
+        return fetch(this.apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        });
+    }
+}
